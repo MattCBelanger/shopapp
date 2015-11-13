e138f861185a40d4297a48fdec2d59d282ebf817
Matt Belanger <matthewcbelanger@gmail.com>
2015-11-13 13:53:52 -0500

    dns

--- app/js/services/ApiService.js
+++ app/js/services/ApiService.js
@@ -7,7 +7,7 @@ angular.module('ShopApp').service('api', ApiService);
 
 
 //constants
-var BASE_URL = "http://localhost:80";
+var BASE_URL = "http://ec2-52-32-60-231.us-west-2.compute.amazonaws.com";
 //var BASE_URL = 'http://174.129.248.23/brainstation/shop';
 var HEADERS = {headers : {'Content-Type': 'application/x-www-form-urlencoded','Authorization':'AFJVJA46AS'}};
 
